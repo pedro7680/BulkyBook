2b57464737bf79374ffdc7cb68f39c4e357b5fd7
Pete Hoben <30258915@cityofglacol.ac.uk>
2020-02-21 19:16:42 +0000

    Add project files.

---
+++ BulkyBook.DataAccess/Repository/CoverTypeRepository.cs
@@ -0,0 +1,30 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class CovertypeRepository : Repository<CoverType>,ICoverTypeRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CovertypeRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(CoverType coverType)
+        {
+            var objFromDb = _db.CoverTypes.FirstOrDefault(s => s.Id == coverType.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Name = coverType.Name;
+               
+            }
+        }
+    }
+}
